94077175fa6b033a41021d44bb2c248762885c46
Gabriel Porto <gabriel_porto@outlook.com>
2024-01-26 01:36:04 -0300

    Removed modal and tabs examples

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -49,10 +49,7 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+      <Stack />
     </ThemeProvider>
   );
 }
