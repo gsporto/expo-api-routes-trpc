902481f085fdb1db99a4ee1a6f8642b1b56839ed
Gabriel Porto <gabriel_porto@outlook.com>
2024-10-04 01:06:14 -0300

    bump trpc@11.0.0-rc.553 @tanstack/react-query@5.59.0 zod@3.23.8 typescript@5.6.2

--- trpc/react.tsx
+++ trpc/react.tsx
@@ -13,14 +13,14 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
 
   const [trpcClient] = useState(() =>
     api.createClient({
-      transformer,
       links: [
         loggerLink({
           enabled: (op) =>
             process.env.NODE_ENV === "development" ||
-            (op.direction === "down" && op.result instanceof Error),
+          (op.direction === "down" && op.result instanceof Error),
         }),
         httpBatchLink({
+          transformer,
           url: getUrl(),
         }),
       ],
